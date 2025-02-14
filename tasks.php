29dbbdf9501bb78320f6c47126f3ce745765227c
Oluodia <123239434+Oluodia@users.noreply.github.com>
2025-02-14 15:25:44 +0300

    Update tasks.php

--- tasks.php
+++ tasks.php
@@ -79,7 +79,7 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="#">To-Do List</a>
+        <a class="navbar-brand" href="/index.php">To-Do List</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
